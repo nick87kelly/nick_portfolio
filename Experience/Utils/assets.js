8ec5d6ff4152db3fd5bfda24cd116fa355d7c2a3
Nick Kelly <nbkelly2@asu.edu>
2022-10-28 13:08:02 -0700

    chnage room

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name: "room",
         type: "glbModel",
-        path: "/models/test.glb"
+        path: "/models/finalRoom.glb"
     },
     {
         name: "tvScreen",
