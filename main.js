03fd094787f08607ed07a409e93b033ba9451b19
Nick Kelly <nbkelly2@asu.edu>
2022-10-29 18:48:56 -0700

    chrome auto scroll bug

--- main.js
+++ main.js
@@ -15,37 +15,3 @@ $(window).scroll(function () {
 });
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
-
-// window.onload = () => {
-//     console.log("ready")
-//     if (!matchMedia('(pointer:fine)').matches) {
-//         var opacityPage = 0;
-//         var intervalIDPage = 0;
-//         setInterval(() => {
-//             var page = document.getElementsByClassName("page")[0];
-//             opacityPage = Number(window.getComputedStyle(page)
-//                 .getPropertyValue("opacity"));
-//             if (opacityPage < 1) {
-//                 opacityPage = opacityPage + 0.1;
-//                 page.style.opacity = opacityPage
-//             } else {
-//                 clearInterval(intervalIDPage);
-//             }
-//         }, 200)
-//     }
-
-//     var opacityLoader = 0;
-//     var intervalIDLoader = 0;
-
-//     setInterval(() => {
-//         var loader = document.getElementsByClassName("loader")[0];
-//         opacityLoader = Number(window.getComputedStyle(loader)
-//             .getPropertyValue("opacity"));
-//         if (opacityLoader > 0) {
-//             opacityLoader = opacityLoader - 0.1;
-//             loader.style.opacity = opacityLoader
-//         } else {
-//             clearInterval(intervalIDLoader);
-//         }
-//     }, 100)
-// }
